de0518692b6838caf50658929e8e4709113b0d8d
Juno Kim <lippoint.surf0622@gmail.com>
2023-08-01 00:29:44 +0900

    add chain localhost

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import {
   optimism,
   polygon,
   zora,
+  localhost
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
@@ -20,7 +21,7 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     optimism,
     arbitrum,
     zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli, localhost] : []),
   ],
   [publicProvider()]
 );
