c0b08c678897ae8e1728a07560e2b79879e1948e
Juno Kim <lippoint.surf0622@gmail.com>
2023-08-05 09:02:43 +0900

    create layout component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,52 +1,36 @@
-import { Box, Button, Divider, Flex } from '@chakra-ui/react';
+import MintButton from '../button/MintButton';
 import { Head, MetaProps } from './Head';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useIsMounted } from '@/hooks/useIsMounted';
 
 interface LayoutProps {
   children: React.ReactNode;
   customMeta?: MetaProps;
 }
 
-export const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
-  const innerHeaderBoxStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#333',
-  };
+export const Layout = ({
+  children,
+  customMeta,
+}: LayoutProps): JSX.Element | undefined => {
+  const { isMounted } = useIsMounted();
 
-  return (
-    <>
-      <Head customMeta={customMeta} />
-      <header>
-        <Flex
-          justifyContent="space-between"
-          alignItems="center"
-          padding={[2, 5, 2, 5]}
-        >
-          <Button
-            as="a"
-            target="_blank"
-            variant="outline"
-            href="https://chakra-ui.com"
-            borderRadius={0}
-          >
-            logo
-          </Button>
-          <Button
-            as="a"
-            target="_blank"
-            variant="outline"
-            href="https://chakra-ui.com"
-            borderRadius={0}
-          >
-            ウォレット接続
-          </Button>
-        </Flex>
-      </header>
-      <Divider />
-      {children}
-    </>
-  );
+  if (isMounted) {
+    return (
+      <>
+        <Head customMeta={customMeta} />
+        <header className="mx-auto px-5 py-5">
+          <div className="flex flex-row justify-between">
+            <div className="min-w-100 outline outline-1 px-10 py-2">logo</div>
+            {/* wallet connect */}
+            <MintButton type="top" />
+            {/* // wallet connect */}
+          </div>
+        </header>
+        <div className=" w-full divide-y divide-black divide-solid" />
+        <div className="container mx-auto flex flex-col justify-center items-center px-7">
+          {children}
+        </div>
+      </>
+    );
+  }
 };
