596864a24f0478e3f80f8c74a0bd58947cfb5669
Juno Kim <lippoint.surf0622@gmail.com>
2023-08-07 14:24:32 +0900

    modify reducer about mint status

--- store/modules/MintStatusReducer.ts
+++ store/modules/MintStatusReducer.ts
@@ -2,17 +2,46 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   isLoading: false,
+  isSuccess: false,
+  isError: false,
+  modalVisible: false,
 };
 
 const mintStatusSlice = createSlice({
   name: 'mintStatusInfo',
   initialState,
   reducers: {
-    updateMintStatus: (state, action) => {
+    updateMintLoadingStatus: (state, action) => {
       state.isLoading = action.payload;
+      state.isSuccess = false;
+      state.isError = false;
+      state.modalVisible = true;
+    },
+    updateMintSuccessStatus: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = action.payload;
+      state.isError = false;
+      state.modalVisible = true;
+    },
+    updateMintErrorStatus: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = action.payload;
+      state.modalVisible = true;
+    },
+    updateMintModalVisible: (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.modalVisible = action.payload;
     },
   },
 });
 
-export const { updateMintStatus } = mintStatusSlice.actions;
+export const {
+  updateMintLoadingStatus,
+  updateMintSuccessStatus,
+  updateMintErrorStatus,
+  updateMintModalVisible,
+} = mintStatusSlice.actions;
 export default mintStatusSlice.reducer;
