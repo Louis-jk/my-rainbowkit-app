401b741c3e2bfef04bab7f5a3711d433b6cc47e0
Juno Kim <lippoint.surf0622@gmail.com>
2023-08-07 14:27:40 +0900

    create constant about api

---
+++ constant/apiKeys.ts
@@ -0,0 +1,4 @@
+export const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || '';
+export const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY || '';
+export const WALLET_CONNECT_PROJECT_ID =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '';
